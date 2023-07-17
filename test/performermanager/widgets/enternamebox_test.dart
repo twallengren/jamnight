012b143f2b39f0ac0cd76f8340cafc6d84643115
toren.wallengren <toren.wallengren@gmail.com>
2023-07-17 09:56:17 +0100

    performermanager widgets

---
+++ test/performermanager/widgets/enternamebox_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jamnight/performermanager/widgets/enternamebox.dart';
+
+void main() {
+  group('EnterNameBox widget', () {
+    testWidgets('initializes without error', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+              body: EnterNameBox(nameController: TextEditingController()))));
+    });
+  });
+}
